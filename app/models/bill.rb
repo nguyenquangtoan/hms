dc5da00ad5066aa04c290464888bc3b56111f840
Phuc Do <dduyphuc@gmail.com>
2011-10-17 08:42:08 +0700

    finish bills calculation

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,6 +2,13 @@ class Bill < ActiveRecord::Base
 	belongs_to :guest
   has_many :bill_details, :dependent => :delete_all
   has_many :occupancies, :through => :bill_details
-  validates :guest_id, :total, :presence => true
+  validates :guest_id, :presence => true
   
+  def total
+    total = 0.0
+    bill_details.each do |b|
+      total += b.amount
+    end
+    return total
+  end
 end
--- app/models/bill_detail.rb
+++ app/models/bill_detail.rb
@@ -1,7 +1,7 @@
 class BillDetail < ActiveRecord::Base
   belongs_to :bill
-  belongs_to :occupancy
+  belongs_to :room
   
-  validates :bill_id, :occupancy_id, :destination, :call_time, :duration, :amount, :presence => true
-  validates :begin_sencond_block, :begin_block_rate, :minute_rate, :fixed_rate, :duration, :numericality => true
+  validates :bill_id, :room_id, :destination, :call_time, :duration, :amount, :presence => true
+  validates :begin_second_block, :begin_block_rate, :minute_rate, :fixed_rate, :duration, :numericality => true
 end
